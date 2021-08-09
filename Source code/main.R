4806939648b82fd5419feffa7154132952df5942
charan-pinninty7 <88632927+charan-pinninty7@users.noreply.github.com>
2021-08-08 22:58:16 -0400

    Update main.R

--- Source code/main.R
+++ Source code/main.R
@@ -5,8 +5,82 @@
 #Ver:1.2
 #Last modification:6/15/2018
 
+#Project Name: Studing the Dementia Caregiver Sleep
+#File Name: Catch Data From E4 for caregiver sleep study
+#Author: Reza Sadeghi
+#Email: sadeghi.2@wright.edu; reza@knoesis.org
+#Ver:1.2
+#Last modification:07/01/2021 by Charan Pinninty (pinninty.2@wright.edu)
+
+memory.limit(size = 16000)
+
+
+person=c(1:18)
+week=c(1:2)
+day=c(1:9)
+signal=list('ACC','BVP','EDA','HR','IBI','Temp')
+data=list(person,week,day,signal)
+
+
+data <- vector(mode = "list", length = 8)
+for (p_i in 1:18){
+  data[[p_i]] <- vector(mode="list", length = 2)
+  for (w_i in 1:2){
+    data[[p_i]][[w_i]] <- vector(mode="list", length = 7)
+    for (d_i in 1:9){
+      data[[p_i]][[w_i]][[d_i]] <- structure(vector(mode="list", length = 6), names= signal)
+    }
+  }
+}
+
+root="C:\\Sleep-quality-in-caregivers-master\\Examine_your_sleep_with_our_model\\Data\\";
+for (p_i in 1:18){
+  for (w_i in 1:2){
+    for (d_i in 1:9){
+      possibleError <- tryCatch({
+        direction=paste(root,'Participant ',as.character(p_i),'\\Week ', as.character(w_i),'\\',as.character(d_i), sep = '')
+        setwd(direction)
+        Flage<<-TRUE
+      },error= function(e){
+        print("There is no such directory")
+        print(direction)
+        Flage<<-FALSE
+      })
+      if(inherits(possibleError, "error")) next
+      #setwd("C:\\Users\\Reza Sadeghi\\Desktop\\Dementia Caregiver Sleep Dataset\\Dataset\\Participant 1\\Week 1\\1")
+      if(!Flage)
+      {
+        next
+      }
+      print(direction)
+      ACC <- read.csv("ACC.csv",header = F,as.is = T)
+      data [[p_i]][[w_i]][[d_i]]$ACC <- ACC
+      
+      BVP <- read.csv("BVP.csv",header = F,as.is = T)
+      data [[p_i]][[w_i]][[d_i]]$BVP <- BVP
+      
+      EDA <- read.csv("EDA.csv",header = F,as.is = T)
+      data [[p_i]][[w_i]][[d_i]]$EDA <- EDA
+      
+      HR <- read.csv("HR.csv",header = F,as.is = T)
+      data [[p_i]][[w_i]][[d_i]]$HR <- HR
+      
+      IBI <- read.csv("IBI.csv",header = F,as.is = T)
+      data [[p_i]][[w_i]][[d_i]]$IBI <- IBI
+      
+      Temp <- read.csv("Temp.csv",header = F,as.is = T)
+      data [[p_i]][[w_i]][[d_i]]$Temp <- Temp
+    }
+  }
+}
+
+
+
+
+library(readxl)
+survey<-read_xlsx("C:\\Sleep-quality-in-caregivers-master\\Examine_your_sleep_with_our_model\\Data\\survey_excel.xlsx")
 # specify the work space
-setwd("C:\\Users\\Reza Sadeghi\\Desktop\\Dementia Caregiver Sleep Dataset")
+setwd("C:\\Sleep-quality-in-caregivers-master\\Examine_your_sleep_with_our_model\\Data")
 
 #>>> Catch Data From E4 for caregiver sleep study
 #CatchDataFromE4_CaregiverSleep
@@ -47,9 +121,16 @@ matlab <- Matlab()
 isOpen <- open(matlab)
 print(matlab)
 
-for (p_i in 1:8){
+root = "C:\\Sleep-quality-in-caregivers-master\\Source code\\"
+
+source(paste(root,'ACC_Sleep.R', sep = ''))
+source(paste(root,'EDA.R', sep = ''))
+source(paste(root,'HRV_SWS.R', sep = ''))
+source(paste(root,'Temp_Sleep.R', sep = ''))
+
+for (p_i in 1:18){
   for (w_i in 1:2){
-    for (d_i in 1:8){
+    for (d_i in 1:9){
       
       #>>>>>>>>>Computing the SWS drived from HRV
       print(paste('participant:',p_i, 'weak:',w_i,'day:', d_i))
